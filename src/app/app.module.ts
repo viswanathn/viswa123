25a7eab10502c7551f8ace307907d975d43f1e3b
Viswanath Telagamsetty <tviswanath@pkrmsezdc.com>
2019-10-22 10:48:42 -0600

    commiting changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //import {MainModule} from 'my-app/src/app/app.module';
-import { MessageComponent} from 'my-app/src/app/message.component';
+import { MainModule} from 'my-app';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent
+   
   ],
   imports: [
     BrowserModule,
+    MainModule
    // MainModule
   ],
   providers: [],
